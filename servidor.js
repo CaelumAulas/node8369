06195a850fa052db0aa6f49641c941dfd1e66263
Artur Diniz Adam <artur.daz@gmail.com>
2019-05-14 20:27:13 -0300

    Porta do ambiente

--- servidor.js
+++ servidor.js
@@ -3,12 +3,14 @@ const express = require("express")
 // Request listener
 const servidor = express()
 
+const porta = process.env.PORTA || 3000
+
 // Assíncrono
 // Listener
 // Função de callback
 // Handler
 // Função anônima
-servidor.listen(3000, function(){
-    console.log("Servidor rodando na porta " + servidor.port)
+servidor.listen(porta, function(){
+    console.log("Servidor rodando na porta " + porta)
 })
 
